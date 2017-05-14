fd61bf69c11b1612f455d7333b275cf5be477731
Simon Walker <s.r.walker101@googlemail.com>
2017-05-14 17:50:51 +0100

    Add Prob trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+pub trait Prob {
+    fn lnlike(&self, params: &[f32]) -> f32;
+    fn lnprior(&self, params: &[f32]) -> f32;
+
+    fn lnprob(&self, params: &[f32]) -> f32 {
+        let lnp = self.lnprior(params);
+        if lnp.is_finite() {
+            lnp + self.lnlike(params)
+        } else {
+            std::f32::INFINITY
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     #[test]
